5167114033cd457785e797a6767feabdb6a1b10d
oth007 <oyvin.h.thuv@uit.no>
2018-04-05 14:55:06 +0200

    Adding missing exp code and synapse code.

--- src/ctrnn/core.clj
+++ src/ctrnn/core.clj
@@ -12,17 +12,30 @@
   )
 
 (defn make-neuron []
-  {:membrane-potential 0
+  {:bias 0
+   :synapses []
+   :membrane-potential 0
    :time-constant 0})
 
+(defn make-synapse [from-neuron]
+  {:from-neuron from-neuron
+   :strength 0
+   })
+  
 (defn add-neuron
   "Returns a network with a new neuron added to it."
   [net neuron]
-  (assoc :neurons (conj (net :neurons) neuron)))
+  (assoc net :neurons (conj (net :neurons) neuron)))
+
+(defn add-synapse
+  "Returns a neuron with a new synaptic connection added"
+  [neuron synapse]
+  (assoc neuron :synapses (conj (neuron :synapses) synapse)))
 
 (defn firing-frequency [neuron]
   "Calculate firing frequency based on current membrane potential."
-  (/ 1 (+ 1 (exp (- (+ (neuron :membrane-potential) (bias neuron)))))))
+  (/ 1 (+ 1 (java.lang.Math/exp
+             (- (+ (neuron :membrane-potential) (neuron :bias)))))))
 
 (defn next-membrane-potential
   "Calculate what the membrane potential will be after timestep."
@@ -32,15 +45,16 @@
            (+ (- (neuron :membrane-potential))
               (neuron :external-current)
               (reduce (fn [in-sum syn]
-                        (* (strength syn)
+                        (* (syn :strength)
                            ((syn :from-neuron)
                             (firing-frequency neuron))))))))))
 
 (defn update-membrane-potentials
-  "Return a network after synchronously updating all membrane potentials."
-  [net]
-  (assoc net :neurons (map (fn [neuron]
-                             (assoc neuron :membrane-potential
-                                    (next-membrane-potential neuron timestep)))
-                           (net :neurons))))
+  "Return a net after synchronously updating all membrane potentials."
+  [net timestep]
+  (assoc net :neurons
+         (map (fn [neuron]
+                (assoc neuron :membrane-potential
+                       (next-membrane-potential neuron timestep)))
+              (net :neurons))))
 
