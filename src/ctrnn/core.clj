4e7e14dcb8b2c71d0aa259d16883d05ddad274fa
Øyvin Halfdan Thuv <oyvin.h.thuv@uit.no>
2018-04-22 00:40:40 +0200

    Simplifying by just using maps for neurons, synapses etc.

--- src/ctrnn/core.clj
+++ src/ctrnn/core.clj
@@ -1,25 +1,14 @@
 (ns ctrnn.core
   (:require [ctrnn.neuron :as neuron]))
 
-(defprotocol CTRNNP
-  (add-neuron [net ^ctrnn.neuron.Neuron neuron])
-  (future-ctrnn [net]
-    "Synchronously update all neurons one timestep")
-  (^clojure.lang.PersistentVector neurons [net]))
+(defn make-ctrnn [neurons timestep]
+  {:neurons neurons
+   :timestep timestep})
 
-(defrecord CTRNN
-    [;; Initial list of neurons
-     neurons
-     ;; Resolution (e.g. number of milliseconds per timestep) that should
-     ;; always be kept smaller than any time-constants set in neurons
-     ^Double timestep
-     ]
-  CTRNNP
-  (add-neuron [net neuron]
-    (assoc net :neurons (conj (:neurons net) neuron)))
-  (future-ctrnn [net]
-    (assoc net :neurons
-           (map (fn [n]
-                  (neuron/update-membrane-potential n (:timestep net)))
-                (:neurons net))))
-  (neurons [net] (:neurons net)))
+(defn update-ctrnn [net]
+  (assoc net :neurons
+         (reduce-kv
+          (fn [m k v]
+            (assoc m k (neuron/update-membrane-potential v net)))
+          {}
+          (:neurons net))))
--- src/ctrnn/neuron.clj
+++ src/ctrnn/neuron.clj
@@ -1,48 +1,43 @@
 (ns ctrnn.neuron)
 
-(defprotocol NeuronP
-  (add-synapse [n from-neuron strength]
-    "Add a synaptic connection from another neuron")
-  (activation [n]
-    "Firing frequency (activation) for current mebrane potential and bias")
-  (set-bias [n b]
-    "Set neuron bias")
-  (set-external-current [n i]
-    "Set external current")
-  (update-membrane-potential [n t]
-    "Approximate membrane potential for step t forward in time"))
+(defn sigmoid [x]
+  (/ 1 (+ 1 (java.lang.Math/exp (- x)))))
 
-(defn input-sum [neuron]
-  (reduce (fn [in-sum synapse]
-            (+ in-sum
-               (* (:strength synapse)
-                  (activation
-                   (:from-neuron synapse)))))
-          0
-          (:synapses neuron)))
-  
-(defrecord Neuron
-    [bias ; Sensivity to input
-     external-current
-     membrane-potential
-     time-constant] ; How fast change happens (should be > than net timestep)
-  NeuronP
-  (add-synapse [n from-neuron strength]
-    (assoc n :synapses
-           (conj (:synapses n) {:from-neuron from-neuron :strength strength})))
-  (activation [n]
-    (/ 1 (+ 1 (java.lang.Math/exp (- (+ (:membrane-potential n) (:bias n)))))))
-  (set-bias [n b]
-    (assoc n :bias b))
-  (set-external-current [n i]
-    (assoc n :external-current i))
-  (update-membrane-potential [n t]
-    (assoc n :membrane-potential
-           (+ (:membrane-potential n)
-              (* t
-                 (/ (+ (- (:membrane-potential n))
-                       (:external-current n)
-                       (input-sum n))
-                    (:time-constant n)))))))
+(defn activation [n]
+  (sigmoid (+ (:membrane-potential n) (:bias n))))
+
+(defn input-sum [neuron net]
+  (let [neurons (:neurons net)]
+    (reduce (fn [in-sum synapse]
+              (+ in-sum
+                 (* (:strength synapse)
+                    (activation ((keyword (:from-neuron-id synapse))
+                                 neurons)))))
+            0
+            (:synapses neuron))))
+
+(defn make-neuron [id bias membrane-potential time-constant]
+  {:bias bias
+   :id id
+   :external-current 0
+   :membrane-potential membrane-potential
+   :time-constant time-constant
+   :synapses []})
+
+(defn add-synapse [neuron from-neuron-id strength]
+  (assoc neuron :synapses
+         (conj (:synapses neuron)
+               {:from-neuron-id from-neuron-id
+                :strength strength})))
+
+(defn update-membrane-potential [n net]
+  (assoc n :membrane-potential
+         (+ (:membrane-potential n)
+            (* (:timestep net)
+               (/ (+ (- (:membrane-potential n))
+                     (:external-current n)
+                     (input-sum n net))
+                  (:time-constant n))))))
+
 
 
